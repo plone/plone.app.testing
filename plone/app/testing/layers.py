d42c19d10b50541342108c8a4d911cc60f92672e
Rok Garbas <rok.garbas@gmail.com>
2010-08-11 23:32:01 +0000

    loading plone.app.blob zcml to be able to create Image and File types in tests.
    svn path=/plone.app.testing/trunk/; revision=38695

--- plone/app/testing/layers.py
+++ plone/app/testing/layers.py
@@ -8,197 +8,198 @@ from plone.app.testing.interfaces import (
         PLONE_SITE_ID,
         PLONE_SITE_TITLE,
         DEFAULT_LANGUAGE,
-        
+
         TEST_USER_NAME,
         TEST_USER_PASSWORD,
         TEST_USER_ROLES,
-        
+
         SITE_OWNER_NAME,
         SITE_OWNER_PASSWORD
     )
 
 class PloneFixture(Layer):
     """This layer sets up a basic Plone site, with:
-    
+
     * No content
     * No default workflow
     * One user, as found in the constant ``TEST_USER_NAME``, with the password
       ``TEST_USER_PASSWORD``, and a single role, ``Member``.
     """
-    
+
     defaultBases = (z2.STARTUP,)
-    
+
     # Products that will be installed, plus options
     products = (
             ('Products.GenericSetup'                , {'loadZCML': True},),
             ('Products.DCWorkflow'                  , {'loadZCML': True}, ),
             ('Products.ZCTextIndex'                 , {'loadZCML': True}, ),
-                                                    
+
             ('Products.CMFActionIcons'              , {'loadZCML': True}, ),
             ('Products.CMFUid'                      , {'loadZCML': True}, ),
             ('Products.CMFCalendar'                 , {'loadZCML': True}, ),
-                                                    
+
             ('Products.CMFCore'                     , {'loadZCML': True},),
             ('Products.CMFDefault'                  , {'loadZCML': True}, ),
-                                                    
+
             ('Products.PluggableAuthService'        , {'loadZCML': True}, ),
             ('Products.PluginRegistry'              , {'loadZCML': True}, ),
             ('Products.PlonePAS'                    , {'loadZCML': True}, ),
-                                                    
+
             ('Products.CMFQuickInstallerTool'       , {'loadZCML': True}, ),
             ('Products.CMFFormController'           , {'loadZCML': True}, ),
             ('Products.CMFDynamicViewFTI'           , {'loadZCML': True}, ),
-                                                    
+
             ('Products.Archetypes'                  , {'loadZCML': True}, ),
             ('Products.MimetypesRegistry'           , {'loadZCML': True}, ),
             ('Products.PortalTransforms'            , {'loadZCML': True}, ),
-                                                    
+
             ('Products.ATContentTypes'              , {'loadZCML': True}, ),
             ('Products.ATReferenceBrowserWidget'    , {'loadZCML': True}, ),
-                                                    
+
             ('Products.ExternalEditor'              , {'loadZCML': True}, ),
             ('Products.ExtendedPathIndex'           , {'loadZCML': True}, ),
             ('Products.ResourceRegistries'          , {'loadZCML': True}, ),
             ('Products.SecureMailHost'              , {'loadZCML': True}, ),
-                                                    
+
             ('Products.PasswordResetTool'           , {'loadZCML': True}, ),
-                                                    
+
             ('Products.CMFPlacefulWorkflow'         , {'loadZCML': True}, ),
             ('Products.kupu'                        , {'loadZCML': True}, ),
             ('Products.TinyMCE'                     , {'loadZCML': True}, ),
-                                                    
+
             ('Products.CMFEditions'                 , {'loadZCML': True}, ),
             ('Products.CMFDiffTool'                 , {'loadZCML': True}, ),
-            
+
             ('Products.PlacelessTranslationService' , {'loadZCML': True}, ),
             ('Products.PloneLanguageTool'           , {'loadZCML': True}, ),
-                                                    
+
             ('plonetheme.classic'                   , {'loadZCML': True}, ),
             ('plonetheme.sunburst'                  , {'loadZCML': True}, ),
-                                                    
+
             ('Products.CMFPlone'                    , {'loadZCML': True}, ),
+            ('plone.app.blob'                       , {'loadZCML': True}, ),
         )
-    
+
     # Extension profiles to be installed with site setup
     extensionProfiles = (
             'plonetheme.sunburst:default',
         )
-    
+
     # Layer lifecycle
-    
+
     def setUp(self):
-        
+
         # Stack a new DemoStorage on top of the one from z2.STARTUP.
         self['zodbDB'] = zodb.stackDemoStorage(self.get('zodbDB'), name='PloneFixture')
-        
+
         # Keep track of the GenericSetup registries so that we can snapshot
         # the changes
         from Products.GenericSetup.registry import _profile_registry
         from Products.GenericSetup.registry import _import_step_registry
         from Products.GenericSetup.registry import _export_step_registry
-        
+
         preSetupProfiles    = list(_profile_registry._profile_ids)
         preSetupImportSteps = list(_import_step_registry.listSteps())
         preSetupExportSteps = list(_export_step_registry.listSteps())
-        
+
         self.setUpZCML()
-        
+
         # Set up products and the default content
         with z2.zopeApp() as app:
             self.setUpProducts(app)
             self.setUpDefaultContent(app)
-        
+
         # Record the changes to the GenericSetup registries
         self.snapshotProfileRegistry(preSetupProfiles, preSetupImportSteps, preSetupExportSteps)
-    
+
     def tearDown(self):
-        
+
         # Tear down products
         with z2.zopeApp() as app:
             # note: content tear-down happens by squashing the ZODB
             self.tearDownProducts(app)
-        
+
         self.tearDownZCML()
         self.tearDownProfileRegistry()
-        
+
         # Zap the stacked ZODB
         self['zodbDB'].close()
         del self['zodbDB']
-    
+
     def snapshotProfileRegistry(self, preSetupProfiles, preSetupImportSteps, preSetupExportSteps):
         """Save a snapshot of all profiles that were added during setup, by
         comparing to the list of profiles passed in.
         """
-        
+
         self._addedProfiles = set()
         self._addedImportSteps = set()
         self._addedExportSteps = set()
-        
+
         from Products.GenericSetup.registry import _profile_registry
         from Products.GenericSetup.registry import _import_step_registry
         from Products.GenericSetup.registry import _export_step_registry
-        
+
         for profileId in _profile_registry._profile_ids:
             if profileId not in preSetupProfiles:
                 self._addedProfiles.add(profileId)
-        
+
         for stepId in _import_step_registry.listSteps():
             if stepId not in preSetupImportSteps:
                 self._addedImportSteps.add(stepId)
-        
+
         for stepId in _export_step_registry.listSteps():
             if stepId not in preSetupExportSteps:
                 self._addedExportSteps.add(stepId)
-        
+
     def tearDownProfileRegistry(self):
         """Delete all profiles that were added during setup, as stored by
         ``snapshotProfileRegistry()``.
         """
-        
+
         from Products.GenericSetup.registry import _profile_registry
         from Products.GenericSetup.registry import _import_step_registry
         from Products.GenericSetup.registry import _export_step_registry
-        
+
         for profileId in self._addedProfiles:
             if profileId in _profile_registry._profile_ids:
                 _profile_registry._profile_ids.remove(profileId)
             if profileId in _profile_registry._profile_info:
                 del _profile_registry._profile_info[profileId]
-        
+
         for stepId in self._addedImportSteps:
             if stepId in _import_step_registry.listSteps():
                 _import_step_registry.unregisterStep(stepId)
-        
+
         for stepId in self._addedExportSteps:
             if stepId in _export_step_registry.listSteps():
                 _export_step_registry.unregisterStep(stepId)
-    
+
     def setUpZCML(self):
         """Stack a new global registry and load ZCML configuration of Plone
         and the core set of add-on products into it. Also set the
         ``disable-autoinclude`` ZCML feature so that Plone does not attempt to
         auto-load ZCML using ``z3c.autoinclude``.
         """
-        
+
         # Create a new global registry
         zca.pushGlobalRegistry()
-        
+
         from zope.configuration import xmlconfig
         self['configurationContext'] = context = zca.stackConfigurationContext(self.get('configurationContext'))
-        
+
         # Turn off z3c.autoinclude
-        
+
         xmlconfig.string("""\
 <configure xmlns="http://namespaces.zope.org/zope" xmlns:meta="http://namespaces.zope.org/meta">
     <meta:provides feature="disable-autoinclude" />
 </configure>
 """, context=context)
-        
+
         # Load dependent products's ZCML - Plone doesn't specify dependencies
         # on Products.* packages fully
-        
+
         from zope.dottedname.resolve import resolve
-        
+
         def loadAll(filename):
             for p, config in self.products:
                 if not config['loadZCML']:
@@ -211,36 +212,36 @@ class PloneFixture(Layer):
                     xmlconfig.file(filename, package, context=context)
                 except IOError:
                     pass
-        
+
         loadAll('meta.zcml')
         loadAll('configure.zcml')
         loadAll('overrides.zcml')
-    
+
     def tearDownZCML(self):
         """Pop the global component registry stack, effectively unregistering
         all global components registered during layer setup.
         """
         # Pop the global registry
         zca.popGlobalRegistry()
-        
+
         # Zap the stacked configuration context
         del self['configurationContext']
-        
+
     def setUpProducts(self, app):
         """Install all old-style products listed in the the ``products`` tuple
         of this class.
         """
-        
+
         for p, config in self.products:
             z2.installProduct(app, p)
-    
+
     def tearDownProducts(self, app):
         """Uninstall all old-style products listed in the the ``products``
         tuple of this class.
         """
         for p, config in reversed(self.products):
             z2.uninstallProduct(app, p)
-        
+
         # Clean up Wicked turds
         # XXX: This may tear down too much state
         try:
@@ -248,18 +249,18 @@ class PloneFixture(Layer):
             meta.cleanUp()
         except ImportError:
             pass
-        
+
     def setUpDefaultContent(self, app):
         """Add the site owner user to the root user folder and log in as that
         user. Create the Plone site, installing the extension profiles listed
         in the ``extensionProfiles`` layer class variable. Create the test
         user inside the site, and disable the default workflow.
-        
+
         Note: There is no explicit tear-down of this setup operation, because
         all persistent changes are torn down when the stacked ZODB
         ``DemoStorage`` is popped.
         """
-        
+
         # Create the owner user and "log in" so that the site object gets
         # the right ownership information
         app['acl_users'].userFolderAddUser(
@@ -268,9 +269,9 @@ class PloneFixture(Layer):
                 ['Manager'],
                 []
             )
-        
+
         z2.login(app['acl_users'], SITE_OWNER_NAME)
-        
+
         # Create the site with the default set of extension profiles
         from Products.CMFPlone.factory import addPloneSite
         addPloneSite(app, PLONE_SITE_ID,
@@ -279,10 +280,10 @@ class PloneFixture(Layer):
                 default_language=DEFAULT_LANGUAGE,
                 extension_ids=self.extensionProfiles,
             )
-        
+
         # Turn off default workflow
         app[PLONE_SITE_ID]['portal_workflow'].setDefaultChain('')
-        
+
         # Create the test user
         app[PLONE_SITE_ID]['acl_users'].userFolderAddUser(
                 TEST_USER_NAME,
@@ -290,7 +291,7 @@ class PloneFixture(Layer):
                 TEST_USER_ROLES,
                 []
             )
-        
+
         # Log out again
         z2.logout()
 
@@ -302,62 +303,62 @@ PLONE_FIXTURE = PloneFixture()
 class PloneTestLifecycle(object):
     """Mixin class for Plone test lifecycle. This exposes the ``portal``
     resource and resets the environment between each test.
-    
+
     This class is used as a mixing for the IntegrationTesting and
     FunctionalTesting classes below, which also mix in the z2 versions of
     the same.
     """
-    
+
     defaultBases = (PLONE_FIXTURE,)
-    
+
     def testSetUp(self):
         super(PloneTestLifecycle, self).testSetUp()
-        
+
         self['portal'] = portal = self['app'][PLONE_SITE_ID]
         self.setUpEnvironment(portal)
-    
+
     def testTearDown(self):
         self.tearDownEnvironment(self['portal'])
         del self['portal']
-        
+
         super(PloneTestLifecycle, self).testTearDown()
-    
+
     def setUpEnvironment(self, portal):
         """Set up the local component site, reset skin data and log in as
         the test user.
         """
-        
+
         # Set up the local site manager
         from zope.site.hooks import setSite
         setSite(portal)
-        
+
         # Reset skin data
         portal.clearCurrentSkin()
         portal.setupCurrentSkin(portal.REQUEST)
-        
+
         # Pseudo-login as the test user
         from plone.app.testing import helpers
         helpers.login(portal, TEST_USER_NAME)
-    
+
     def tearDownEnvironment(self, portal):
         """Log out, invalidate standard RAM caches, and unset the local
         component site to clean up after tests.
         """
-        
+
         # Clear the security manager
         from plone.app.testing import helpers
         helpers.logout()
-        
+
         # Clear any cached data using plone.memoize's RAM caches
         from plone.memoize.ram import global_cache
         global_cache.invalidateAll()
-        
+
         from zope.component import queryUtility
         from plone.memoize.ram import IRAMCache
         cache = queryUtility(IRAMCache)
         if cache is not None:
             cache.invalidateAll()
-        
+
         # Unset the local component site
         from zope.site.hooks import setSite
         setSite(None)
