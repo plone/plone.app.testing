7a8a9eb7fd23ee5cd46e51e59c389b3c51766bb7
Ross Patterson <me@rpatterson.net>
2010-10-30 15:43:01 +0000

    Use the Selenium 2 web driver.  This removes the need to download any jar files or run any Java since the web driver installs an extension into any given browser to run tests.
    svn path=/plone.app.testing/trunk/; revision=40956

--- plone/app/testing/selenium_layers.py
+++ plone/app/testing/selenium_layers.py
@@ -1,10 +1,8 @@
-import selenium
 import os
 import transaction
 import unittest2 as unittest
 
 from plone.app.testing import PloneSandboxLayer
-from plone.app.testing import PLONE_SITE_ID
 from plone.app.testing import FunctionalTesting
 from plone.app.testing.layers import PLONE_FIXTURE
 from plone.testing import z2
@@ -13,20 +11,15 @@ from plone.testing import z2
 class SeleniumLayer(PloneSandboxLayer):
     defaultBases = (z2.ZSERVER_FIXTURE, PLONE_FIXTURE)
 
-    # Connection parameters
-    seleniumHost = os.environ.get('SELENIUM_HOST', 'localhost')
-    seleniumPort = os.environ.get('SELENIUM_PORT', '4444')
-    seleniumBrowser = os.environ.get('SELENIUM_BROWSER', '*firefox')
-
     def setUpPloneSite(self, portal):
         # Start up Selenium
-        url = "http://%s:%s/%s" % (self['host'], self['port'], PLONE_SITE_ID)
-        self['selenium'] = selenium.selenium(
-            self.seleniumHost, self.seleniumPort, self.seleniumBrowser, url)
-        self['selenium'].start()
+        driver = os.environ.get('SELENIUM_DRIVER', 'firefox')
+        webdriver = __import__(
+            'selenium.%s.webdriver' % driver, fromlist=['WebDriver'])
+        self['selenium'] = webdriver.WebDriver()
 
     def tearDownPloneSite(self, portal):
-        self['selenium'].stop()
+        self['selenium'].quit()
         del self['selenium']
 
 SELENIUM_FIXTURE = SeleniumLayer()
@@ -40,15 +33,7 @@ class SeleniumTestCase(unittest.TestCase):
     def setUp(self):
         self.selenium = self.layer['selenium']
 
-    def open(self, path="/", site_name=PLONE_SITE_ID):
+    def open(self, url):
         # ensure we have a clean starting point
         transaction.commit()
-        self.selenium.open("/%s/%s" % (site_name, path,))
-
-    def wait(self, timeout="30000"):
-        self.selenium.wait_for_page_to_load(timeout)
-
-    def waitForElement(self, selector, timeout="30000"):
-        """Continue checking for the element matching the provided CSS
-        selector."""
-        self.selenium.wait_for_condition("""css="%s" """ % selector, timeout)
+        self.selenium.get(url)
