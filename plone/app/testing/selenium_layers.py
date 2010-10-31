a25d2666318ec26c34ce081f1e9bf094848229b1
Ross Patterson <me@rpatterson.net>
2010-10-31 14:57:46 +0000

    The basic selenium layer does not depend on Plone or Zope at all so rework the layer factoring a bit.
    svn path=/plone.app.testing/trunk/; revision=44751

--- plone/app/testing/selenium_layers.py
+++ plone/app/testing/selenium_layers.py
@@ -1,17 +1,16 @@
 import os
 import transaction
 
-from plone.app.testing import PloneSandboxLayer
+from plone.testing import Layer
+from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import FunctionalTesting
-from plone.app.testing.layers import PLONE_FIXTURE
 from plone.app.testing import TEST_USER_NAME, TEST_USER_PASSWORD
 from plone.testing import z2
 
 
-class SeleniumLayer(PloneSandboxLayer):
-    defaultBases = (z2.ZSERVER_FIXTURE, PLONE_FIXTURE)
+class SeleniumLayer(Layer):
 
-    def setUpPloneSite(self, portal):
+    def setUp(self):
         # Start up Selenium
         driver = os.environ.get('SELENIUM_DRIVER', '') or 'firefox'
         webdriver = __import__(
@@ -21,13 +20,17 @@ class SeleniumLayer(PloneSandboxLayer):
                 if arg.strip()]
         self['selenium'] = webdriver.WebDriver(*args)
 
-    def tearDownPloneSite(self, portal):
+    def tearDown(self):
         self['selenium'].quit()
         del self['selenium']
 
 SELENIUM_FIXTURE = SeleniumLayer()
-SELENIUM_TESTING = FunctionalTesting(
-    bases=(SELENIUM_FIXTURE,), name="SeleniumTesting:Functional")
+SELENIUM_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(SELENIUM_FIXTURE, z2.ZSERVER_FIXTURE),
+    name="SeleniumTesting:Functional")
+SELENIUM_PLONE_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(SELENIUM_FIXTURE, z2.ZSERVER_FIXTURE, PLONE_FIXTURE),
+    name="SeleniumTesting:Functional")
 
 
 def open(selenium, url):
