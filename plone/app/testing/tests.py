f79c1706ed09f157362630b7e819b9aa7814bdc2
Ross Patterson <me@rpatterson.net>
2010-10-31 14:54:32 +0000

    pep8
    svn path=/plone.app.testing/trunk/; revision=44749

--- plone/app/testing/tests.py
+++ plone/app/testing/tests.py
@@ -3,15 +3,17 @@ import doctest
 
 OPTIONFLAGS = doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE
 
+
 # Dummy handler used in tests
 def dummy(context):
     pass
+
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([
         doctest.DocFileSuite('cleanup.txt', optionflags=OPTIONFLAGS),
-        doctest.DocFileSuite('layers.txt',  optionflags=OPTIONFLAGS),
+        doctest.DocFileSuite('layers.txt', optionflags=OPTIONFLAGS),
         doctest.DocFileSuite('helpers.txt', optionflags=OPTIONFLAGS),
         doctest.DocFileSuite('selenium.txt', optionflags=OPTIONFLAGS),
     ])
