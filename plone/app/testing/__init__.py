489a7aadfe7c2bc7de662e041232477787fea993
Godefroid Chapelle <gotcha@bubblenet.be>
2011-03-14 13:49:46 +0000

    remove unneeded class
    svn path=/plone.app.testing/trunk/; revision=48065

--- plone/app/testing/__init__.py
+++ plone/app/testing/__init__.py
@@ -4,7 +4,6 @@ from plone.app.testing.layers import (
         PloneTestLifecycle,
         IntegrationTesting,
         FunctionalTesting,
-        ExtensionProfileFixture,
         PLONE_FIXTURE,
 
         PLONE_INTEGRATION_TESTING,
