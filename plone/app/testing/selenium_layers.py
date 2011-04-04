3bb863ad6f5ff16e35c1c08d3d0400baa819dd5c
Ed Manlove <devPyPlTw@verizon.net>
2011-04-04 16:18:43 +0000

    Added helper functions for selenium layer.
    svn path=/plone.app.testing/trunk/; revision=48408

--- plone/app/testing/selenium_layers.py
+++ plone/app/testing/selenium_layers.py
@@ -33,6 +33,7 @@ SELENIUM_PLONE_FUNCTIONAL_TESTING = FunctionalTesting(
     bases=(SELENIUM_FIXTURE, PLONE_FIXTURE),
     name="SeleniumTesting:Functional")
 
+# Helper functions
 
 def open(selenium, url):
     # ensure we have a clean starting point
@@ -46,7 +47,61 @@ def login(selenium, portal, username=False, password=False):
     if not password:
         password = TEST_USER_PASSWORD
     
-    selenium.get(portal.absolute_url() + '/login_form')
+    open(selenium, portal.absolute_url() + '/login_form')
     selenium.find_element_by_name('__ac_name').send_keys(username)
     selenium.find_element_by_name('__ac_password').send_keys(password)
     selenium.find_element_by_name('submit').click()
+
+def click(selenium, xpath):
+    if xpath.count("link="):
+        link = xpath.split("link=")[-1]
+        element = selenium.find_element_by_partial_link_text(link)
+    elif xpath.count("//"):
+        element = selenium.find_element_by_xpath(xpath)
+    elif xpath.count('#'):
+        eleName = xpath.split("#")[-1]
+        element = selenium.find_element_by_id(eleName)
+    else:
+        element = selenium.find_element_by_name(xpath)
+
+    element.click()
+
+def type(selenium, name, value):
+    selenium.find_element_by_name(name).send_keys(value)
+    
+def typeMce(selenium, value):
+    '''
+    Text fields with mce are different.We need to go into the frame and update the 
+    p element to make this work. Unfortunately the code to get out of the frame is not 
+    implemented in python yet. The workaround is to use this handle trick, which 
+    is currently unsupported in chrome. See issue #405 for more. In general there
+    are still a lot of open issues on frame support so if this breaks it won't 
+    be a surprise.'''
+    handle = selenium.get_current_window_handle()
+    selenium.switch_to_frame("form.text_ifr")
+    ele = selenium.find_element_by_xpath("//p")
+    ele.send_keys(value)
+    selenium.switch_to_window(handle)
+
+def clear(selenium, name):
+    selenium.find_element_by_name(name).clear()
+
+def select(selenium, xpath1, xpath2=''):
+    xpath = xpath1
+    if xpath2:
+        xpath = "%s['%s']"%(xpath1, xpath2)
+        xpath = xpath.replace("select['label=", "select/option['text()=")
+    selenium.find_element_by_xpath(xpath).select()
+
+def waitForPageToLoad(selenium, foo):
+    # this does nothing but make us lazy folks happy
+    pass
+
+def publish(selenium):
+    click(selenium, "//dl[@id='plone-contentmenu-workflow']/dt/a")
+    click(selenium, "#workflow-transition-publish")
+    
+def submit(selenium, formId):
+    selenium.find_element_by_id(formId).submit()
+        
+
