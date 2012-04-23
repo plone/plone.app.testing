595b022ff8dc0335c065eac9beaa6a2db1c56a6f
Johannes Raggam <raggam-nl@adm.at>
2012-04-23 17:24:11 +0200

    no zcml in P.DRI

--- plone/app/testing/layers.py
+++ plone/app/testing/layers.py
@@ -80,7 +80,7 @@ class PloneFixture(Layer):
             ('plone.app.imaging'                    , {'loadZCML': True}, ),
             ('plone.app.collection'                 , {'loadZCML': True}, ),
 
-            ('Products.DateRecurringIndex'          , {'loadZCML': True}, ),
+            ('Products.DateRecurringIndex'          , {'loadZCML': False}, ),
 
             ('Products.CMFPlone'                    , {'loadZCML': True}, ),
 
