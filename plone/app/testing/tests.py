131786242b6c555af524d81c3c0d99aec765ce4c
Ross Patterson <me@rpatterson.net>
2013-10-20 15:40:36 -0700

    Re-enable the selenium tests, work just fine for me:
    See 85c2be07c986704b961daa1781769dd035dda073

--- plone/app/testing/tests.py
+++ plone/app/testing/tests.py
@@ -11,14 +11,14 @@ def dummy(context):
 
 def test_suite():
     suite = unittest.TestSuite()
-    #seltest = doctest.DocFileSuite('selenium.rst', optionflags=OPTIONFLAGS)
+    seltest = doctest.DocFileSuite('selenium.rst', optionflags=OPTIONFLAGS)
     # Run selenium tests on level 2, as it requires a correctly configured
     # Firefox browser
-    #seltest.level = 2
+    seltest.level = 2
     suite.addTests([
         doctest.DocFileSuite('cleanup.rst', optionflags=OPTIONFLAGS),
         doctest.DocFileSuite('layers.rst', optionflags=OPTIONFLAGS),
         doctest.DocFileSuite('helpers.rst', optionflags=OPTIONFLAGS),
-#        seltest,
+        seltest,
     ])
     return suite
