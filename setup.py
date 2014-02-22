8fcec4e1ff4e6fae6bba39ae6769e28d9f65aa78
Timo Stollenwerk <contact@timostollenwerk.net>
2014-02-22 07:01:55 +0100

    Pep8

--- setup.py
+++ setup.py
@@ -16,47 +16,48 @@ robot_require = ['robotsuite>=1.4.0',
                  'decorator',
                  'selenium']
 
-setup(name='plone.app.testing',
-      version=version,
-      description="Testing tools for Plone-the-application, based on plone.testing.",
-      long_description=open("README.rst").read() + "\n" +
-                       open("CHANGES.rst").read(),
-      classifiers=[
+setup(
+    name='plone.app.testing',
+    version=version,
+    description="Testing tools for Plone-the-application, based on plone.testing.",
+    long_description=open("README.rst").read() + "\n" +
+                     open("CHANGES.rst").read(),
+    classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        ],
-      keywords='',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='http://pypi.python.org/pypi/plone.app.testing',
-      license='GPL version 2',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'zope.configuration',
-          'zope.component',
-          'zope.dottedname',
-          'zope.site',
-          'zope.testing',
-          'five.localsitemanager',
-          'plone.memoize',
-          'plone.testing [zca,security,zodb,z2]',
-          # 'Acquisition', # Zope 2.13+
-          # 'AccessControl', # Zope 2.13+
-          'Products.CMFPlone',
-          'Products.GenericSetup',
-          'Zope2',
-      ],
-      tests_require=tests_require,
-      extras_require={
+    ],
+    keywords='',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='http://pypi.python.org/pypi/plone.app.testing',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.configuration',
+        'zope.component',
+        'zope.dottedname',
+        'zope.site',
+        'zope.testing',
+        'five.localsitemanager',
+        'plone.memoize',
+        'plone.testing [zca,security,zodb,z2]',
+        # 'Acquisition', # Zope 2.13+
+        # 'AccessControl', # Zope 2.13+
+        'Products.CMFPlone',
+        'Products.GenericSetup',
+        'Zope2',
+    ],
+    tests_require=tests_require,
+    extras_require={
         'test': tests_require,
         'robot': robot_require,
-      },
-      )
+    },
+)
