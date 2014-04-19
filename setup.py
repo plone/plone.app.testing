4f54f7cc8795e3193708ab87bbf51ee19638073c
tisto <tisto@plone.org>
2014-04-19 13:15:27 +0200

    Back to development: 5.0a3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '5.0a2'
+version = '5.0a3.dev0'
 
 tests_require = ['Products.CMFCore',
                  'Products.PluggableAuthService',
