5483d80aac305453fdd8c8eacb95528341365eb4
esteele <eric@esteele.net>
2014-10-23 22:45:16 -0400

    Back to development: 5.0b2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '5.0b1'
+version = '5.0b2.dev0'
 
 tests_require = ['Products.CMFCore',
                  'Products.PluggableAuthService',
