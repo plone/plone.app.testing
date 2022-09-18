8f93055c933abd9d9f9265d36232051cbab455e6
David Glick <david@glicksoftware.com>
2022-09-17 19:10:34 -0700

    Increase test password length

--- src/plone/app/testing/interfaces.py
+++ src/plone/app/testing/interfaces.py
@@ -8,10 +8,10 @@ DEFAULT_LANGUAGE = 'en'
 
 TEST_USER_NAME = 'test-user'
 TEST_USER_ID = 'test_user_1_'
-TEST_USER_PASSWORD = 'secret'
+TEST_USER_PASSWORD = 'correct horse battery staple'
 TEST_USER_ROLES = ['Member', ]
 
 SITE_OWNER_NAME = 'admin'
-SITE_OWNER_PASSWORD = 'secret'
+SITE_OWNER_PASSWORD = 'correct horse battery staple'
 
 ROBOT_TEST_LEVEL = 5
