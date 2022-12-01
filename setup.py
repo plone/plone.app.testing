0a4d2574914e6b496ec87b66675e052e3f8fcc29
Maurits van Rees <maurits@vanrees.org>
2022-12-02 00:44:57 +0100

    Require at least Python 3.7.
    [ci skip]

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setup(
     namespace_packages=['plone', 'plone.app'],
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=3.7",
     install_requires=[
         'setuptools',
         'six',
