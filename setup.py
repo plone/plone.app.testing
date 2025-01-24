6bf7bd6fac5e121e6b2458951d77e5b159ea7499
Maurits van Rees <maurits@vanrees.org>
2025-01-24 11:54:38 +0100

    Preparing release 7.1.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "7.1.1.dev0"
+version = "7.1.1"
 
 long_description = (
     f"{(Path('docs') / 'README.rst').read_text()}\n"
